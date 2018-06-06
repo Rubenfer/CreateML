3474519e099c85c99e0acb31b79d18cee989671c
Rubén Fernández <9885219+Rubenfer@users.noreply.github.com>
2018-06-06 20:15:51 +0200

    Add files via upload

---
+++ Dog_or_Cat/DogOrCat.playground/Contents.swift
@@ -0,0 +1,7 @@
+import CreateMLUI
+
+let builder = MLImageClassifierBuilder()
+builder.showInLiveView()
+
+
+//You only need to drag and drop the image folders. Then drag and drop the "ImageClassifier" to your project
